a47858e5b4775ce3a7dd1e637902bd3e76bff2ab
Redomine <romankaev@gmail.com>
2020-08-05 17:56:12 +0300

    update
    Multiple connections and canvas display

--- app.js
+++ app.js
@@ -8,3 +8,31 @@ app.get('/',function(req, res) {
 app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(2000);
+console.log("Server started.");
+
+var SOCKET_LIST = {};
+
+var io = require('socket.io')(serv,{});
+
+io.sockets.on('connection', function(socket){
+    console.log('socket connected');
+
+    socket.id = Math.random();
+    socket.x = 0;
+    socket.y = 0;
+    SOCKET_LIST[socket.id] = socket;
+
+});
+
+setInterval(function() {
+    for(var i in SOCKET_LIST){
+      var socket = SOCKET_LIST[i]
+      socket.x++
+      socket.y++
+      
+      socket.emit('newPosition', {
+          x:socket.x,
+          y:socket.y
+      })
+    }
+}, 100/25)
